403f0cedbfc7f6d5e0505e5855a37c94055f5059
unknown <deo_singiza@hotmail.com>
2023-07-17 18:48:01 -0400

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -70,7 +70,7 @@ export default function Home() {
 
             <div
               key={"element_Header_Wrapper"}
-              className=" landscape:overflow-y-scroll  landscape:overflow-x-hidden landscape:pb-20 flex h-full w-full grid-cols-10 flex-col md:grid"
+              className=" landscape:overflow-y-hidden  landscape:overflow-x-hidden landscape:pb-20 flex h-full w-full grid-cols-10 flex-col md:grid"
             >
               <div
                 key={"element_Header_Child1"}
@@ -93,7 +93,7 @@ export default function Home() {
               <div
               
                 key={"element_Header_Slides"}
-                className="col-span-6 flex h-full flex-1 flex-col justify-start p-5 pb-20 md:justify-center md:p-5 mb-9 overflow-x-hidden overflow-y-scroll"              >
+                className="col-span-6 flex h-full flex-1 flex-col justify-start p-5 pb-20 md:justify-center md:p-5 mb-9 overflow-x-hidden md:overflow-y-hidden"              >
               <div className={styles.upnext}> Up Next..</div>
 
                 <Slides data={data} />
