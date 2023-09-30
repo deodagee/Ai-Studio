d0fc4f1b8e1b4c69d7cc83d1c00bbdfcb17ec023
unknown <deo_singiza@hotmail.com>
2023-09-30 19:01:49 -0400

    first commit

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -80,7 +80,7 @@ const SliderButton = ({
   return (
     <button
       className=" flex h-14 w-14 items-center justify-center rounded-full border-[1px] border-[#fdfdfd5f] transition duration-300
-ease-in-out hover:bg-white hover:text-black
+ease-in-out hover:bg-white hover:text-black 
 "
       onClick={handleClick}
     >
